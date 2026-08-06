d0434bc2c6b2b44491ae970765e31bb3867d3d44
Egide Kagiraneza <Kagide6@gmail.com>
2026-08-06 21:31:08 +0000

    feat: create evaluation module

---
+++ evaluate.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import (
+    classification_report,
+    confusion_matrix,
+    precision_recall_fscore_support,
+)
+
+
+def evaluate_model(y_true, y_pred):
+    """Evaluates model predictions."""
+    pass
